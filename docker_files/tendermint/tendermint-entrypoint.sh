c5e10ea78ecf47c6797ecf2dbecac207f64afffb
Antoine <antoine@cercle.co>
2017-07-03 20:03:48 +0200

    fix chmod  + add init

--- docker_files/tendermint/tendermint-entrypoint.sh
+++ docker_files/tendermint/tendermint-entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'postgres' ]; then
-    echo "TOTO"
-
-    if [ -z "$(ls -A "$PGDATA")" ]; then
-        gosu postgres initdb
-    fi
-
-    exec gosu postgres "$@"
+if [ -z "$(ls -A "$PGDATA")" ]; then
+    tendermint init --home=$TMHOME --log_level "debug" 
 fi
 
-exec "$@"
+exec tendermint node --proxy_app=tcp://${PROXY_IP}:46658 --home=$TMHOME --log_level "debug"
