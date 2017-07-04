de11768ffd7681d372abc180fc5fb1834f433a2f
Antoine <antoine@cercle.co>
2017-07-04 08:26:04 +0200

    improve indentation

--- docker_files/tendermint/tendermint-entrypoint.sh
+++ docker_files/tendermint/tendermint-entrypoint.sh
@@ -5,33 +5,32 @@ set -e
 if [ -z "$(ls -A "$PGDATA")" ]; then
     tendermint init --home=$TMHOME --log_level "debug" 
 
-
-## COPY THE PUB KEY INTO THE NGINX SERVER FOLDER
-cp /tendermint/PUB_KEY_HERE.json /pubkey
-
-## CREATE THE GENESIS FILE 
-# fill genesis file with validators
-
-#IFS=',' read -ra VALS_ARR <<< "$VALIDATORS"
-#fqdn_suffix=$(hostname -f | sed 's#[^.]*\.\(\)#\1#')
-#for v in "${VALS_ARR[@]}"; do
-#  # wait until validator generates priv/pub key pair
-#  set +e
-
-#  curl -s "http://$v.$fqdn_suffix/pub_key.json" > /dev/null
-#  ERR=$?
-#  while [ "$ERR" != 0 ]; do
-#    sleep 5
-#    curl -s "http://$v.$fqdn_suffix/pub_key.json" > /dev/null
-#    ERR=$?
-#  done
-#  set -e
-
-#  # add validator to genesis file along with its pub_key
-#  curl -s "http://$v.$fqdn_suffix/pub_key.json" | jq ". as \$k | {pub_key: \$k, amount: $VALIDATOR_POWER, name: \"$v\"}" > pub_validator.json
-#  cat /tendermint/genesis.json | jq ".validators |= .+ [$(cat pub_validator.json)]" > tmpgenesis && mv tmpgenesis /tendermint/genesis.json
-#  rm pub_validator.json
-#done
+	## COPY THE PUB KEY INTO THE NGINX SERVER FOLDER
+	cp /tendermint/PUB_KEY_HERE.json /pubkey
+	
+	## CREATE THE GENESIS FILE 
+	# fill genesis file with validators
+	
+	#IFS=',' read -ra VALS_ARR <<< "$VALIDATORS"
+	#fqdn_suffix=$(hostname -f | sed 's#[^.]*\.\(\)#\1#')
+	#for v in "${VALS_ARR[@]}"; do
+	#  # wait until validator generates priv/pub key pair
+	#  set +e
+	
+	#  curl -s "http://$v.$fqdn_suffix/pub_key.json" > /dev/null
+	#  ERR=$?
+	#  while [ "$ERR" != 0 ]; do
+	#    sleep 5
+	#    curl -s "http://$v.$fqdn_suffix/pub_key.json" > /dev/null
+	#    ERR=$?
+	#  done
+	#  set -e
+	
+	#  # add validator to genesis file along with its pub_key
+	#  curl -s "http://$v.$fqdn_suffix/pub_key.json" | jq ". as \$k | {pub_key: \$k, amount: $VALIDATOR_POWER, name: \"$v\"}" > pub_validator.json
+	#  cat /tendermint/genesis.json | jq ".validators |= .+ [$(cat pub_validator.json)]" > tmpgenesis && mv tmpgenesis /tendermint/genesis.json
+	#  rm pub_validator.json
+	#done
 
 fi
 
