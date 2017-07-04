a6c0175a97b93cbaeda1df25358a5f46110c234c
Antoine <antoine@cercle.co>
2017-07-04 07:51:33 +0200

    improve docker-compose & add documentation

--- docker_files/tendermint/tendermint-entrypoint.sh
+++ docker_files/tendermint/tendermint-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+## YOU NEED TO DO THE TENDERMINT INIT TO GENERATE PUB_KEY/PRIVATE
 if [ -z "$(ls -A "$PGDATA")" ]; then
     tendermint init --home=$TMHOME --log_level "debug" 
 fi
+
+## YOU NEED TO COPY THE PUB KEY INTO THE NGINX SERVER
+cp /tendermint/genesis.json /pubkey
+
+## YOU NEED TO CREATE THE GENESIS FILE 
 
 exec tendermint node --proxy_app=tcp://${PROXY_IP}:46658 --home=$TMHOME --log_level "debug"
