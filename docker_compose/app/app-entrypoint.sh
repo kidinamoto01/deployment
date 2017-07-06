79d7bfed85a36bd450f0eeeaab9dec303a743998
Antoine <antoine@cercle.co>
2017-07-06 17:04:45 +0200

    app entrypoint in the docker compose

--- docker_compose/app/app-entrypoint.sh
+++ docker_compose/app/app-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x
 
-get get ${GITHUB_REPO}
+go get -v ${GITHUB_REPO}
 
 exec ${COMMAND}
-
-#exec tendermint node --proxy_app=tcp://${PROXY_IP}:46658 --home=$TMHOME --log_level "debug"
