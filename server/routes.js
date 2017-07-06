ff47b35031a2dcbe89940d819783f3c0e73f7b1a
Francois-Xavier Aeberhard <fx@red-agent.com>
2017-07-06 19:49:36 +0000

    Add parameters to terraform outpur

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ var execSync = require('child_process').execSync;
 var fs = require('fs');
 
 var TERRAFORMAPPLY_CMD = 'nohup terraform apply ./instance_tfs > tera.log';
-var TERRAFORMAMI = "ami-a4c7edb2";
+var TERRAFORMAMI = "ami-55525643";
 
 /**
  * List state  for given nodes
@@ -38,16 +38,16 @@ router.get('/list', function(req, res) {
     try {
         var stdout = execSync("terraform refresh ./instance_tfs");
     } catch (e) {
-        console.log("'terraform refresh' failed");
-        console.log(e);
+        console.error("'terraform refresh' failed", e);
         return res.send({ error: "Terraform is not able to perform the requested operation" });
     }
     //console.log("Terraform configuration refreshed");
 
-    getNodesStatus(nodes, function() {
+    getNodesStatus(nodes, function(output) {
         res.send(output);
     });
 });
+
 router.get('/list2', function(req, res) {
     res.send({ nodes: { 1: { IP: "172.31.31.127" } } })
 })
@@ -55,26 +55,30 @@ router.get('/list2', function(req, res) {
 function getNodesStatus(nodes, cb) {
     // Call terraform output to retrieve JSON, process it and return result
     exec("terraform output -json", function(error, stdout, stderr) {
+
+
+        var output = { error: null, nodes: {} };
         try {
             console.log("Terraform output", stdout);
             var nodeData = JSON.parse(stdout);
         } catch (e) {
-            return res.send({ error: "Unable to process Terraform return" });
+            console.error("Unable to process Terraform return");
+            //res.send({ error: "Unable to process Terraform return" });
+            output.error = "Unable to process Terraform return";
+            cb(output);
+            return;
         }
-        var output = { error: null, nodes: {} };
+
+        var ret = {};
+
         for (var i = 0; i < nodes.length; i++) {
-            var node = 'node' + nodes[i]
-            if (typeof nodeData[node + '_ip'] !== 'undefined' && nodeData[node + '_ip']) {
-                output.nodes[node] = {
-                    state: nodeData[node + '_state'].value[0],
-                    IP: nodeData[node + '_ip'].value[0]
-                };
-            } else { //output error
-                output.nodes[node] = {
-                    error: "unknown_node",
-                    state: "",
-                    IP: ""
-                };
+            var node = nodes[i],
+                data = nodeData['node' + node];
+
+            if (data) {
+                output.nodes[node] = data.value;
+            } else {
+                output.nodes[node] = { error: "unknown_node" }
             }
         }
         cb(output);
@@ -85,6 +89,7 @@ function getNodesStatus(nodes, cb) {
  * Create new project
  */
 router.get('/create', function(req, res) {
+    console.log("Running route /create");
 
     if (!validate(req, res, { nodes: { type: 'JSON', required: true }, project: { required: true }, git: { required: true }, app: { required: true } })) return;
 
@@ -109,27 +114,30 @@ router.get('/create', function(req, res) {
             + '  provisioner "remote-exec" {\n' //
             + '    inline = [\n' //
             //+ '        "touch testfile"\n' //
-            + '      "./deployment/docker/init -g=' + git + ' -e=' + app + ' -p=' + project + ' -n=' + node + ' --tendermintPort=46656 --proxyPort=46658 --appPort=46659"\n' //
+            // +'      "./deployment/docker/init -g=' + git + ' -a=' + app + ' -p=' + project + ' -n=' + node + ' --tendermintPort=46656 --proxyPort=46658 --appPort=46659"\n' //
             + '    ]\n' //
             + '    connection= {\n' //
             + '      user ="ec2-user"\n' //
             + '      private_key="${file("/home/ec2-user/.ssh/Multiverseterraform.pem")}"\n' //
             + '    }\n' //
             + '  }\n' //
-            + '}\n\n' + 'output "' + node + '_ip" {\n' //
-            + '  value = ["${aws_instance.' + node + '.public_ip}"]\n' //
             + '}\n\n' //
-            + 'output "' + node + '_state" {\n' //
-            + '  value = ["${aws_instance.' + node + '.instance_state}"]\n' //
+            + 'output "' + node + '" {\n' //
+            + '  value = {' //
+            + '    public_dns = "${aws_instance.' + node + '.public_dns }"\n' //
+            + '    ip = "${aws_instance.' + node + '.public_ip}"\n' //
+            + '    state = "${aws_instance.' + node + '.instance_state}"\n' //
+            + '    availability_zone = "${aws_instance.' + node + '.availability_zone}"\n' //
+            + '  }\n' //
             + '}\n\n';
 
         r += cfg;
 
-        var filepath = __dirname + "/instance_tfs/" + node + ".tf";
+        var filepath = __dirname + "/instance_tfs/tf_" + project + node + ".tf";
         //fs.closeSync(fs.openSync(filepath, 'w'));
         fs.writeFile(filepath, cfg, function(err) {
             if (err) {
-                return console.log(err);
+                return console.error(err);
             }
         });
     }
@@ -138,24 +146,62 @@ router.get('/create', function(req, res) {
 
     exec(TERRAFORMAPPLY_CMD, function(err, stdout, stderr) {
         if (err) {
-            return console.log(err);
+            return console.error(err);
         }
-        return;
-        getNodeStatus(nodes, function(data) {
-            var ips = data.nodes.map(function(o) {
-                return o.IP;
+
+        getNodesStatus(nodes, function(data) {
+            var ips = object_values(data.nodes).map(function(o) {
+                return o.ip;
             }).join(',');
-            console.log("ip list", ips)
+            console.log(data)
+                //console.log("ip list", ips)
             for (var i in data.nodes) {
-                console.log("Running ssh on:", data.nodes[i]);
-                nodes.
-                exec("ssh ec2-user@" + data.nodes[i].IP + ' -m "init -g=' + git + ' -e=' + app + ' -p=' + project + ' -n=' + node + ' -ips=' + ips + '"');
+                var node = data.nodes[i];
+                var cmd = 'cd deployment/docker;./init -g=' + git + ' -a=' + app + ' -p=' + project + ' -n=' + i + ' -i=' + ips + " --tendermintPort=46656 --proxyPort=46658 --appPort=46659";
+                //var cmd = "touch testfile";
+                console.log("Running ssh on:", data.nodes[i], "cmd: ", cmd);
+                //exec("ssh ec2-user@" + data.nodes[i].IP + '  "init -g=' + git + ' -e=' + app + ' -p=' + project + ' -n=' + node + ' -ips=' + ips + '"');
+                exec("ssh -oStrictHostKeyChecking=no ec2-user@" + node.ip + ' "' + cmd + '"', function(e, stdout, stderr) {
+                    if (e) console.error(e, stdout, stderr);
+                });
+                // exec("ssh ec2-user@" + data.nodes[i].IP + ' -m ' //
+                //     + '"./deployment/docker/init -g=' + git + ' -a=' + app + ' -p=' + project + ' -n=' + node + ' --tendermintPort=46656 --proxyPort=46658 --appPort=46659 -i=' + ips + '"')
             }
         });
-        //console.log("Terraform output", stdout, err, stdout);
     });
 
     res.send({});
+});
+
+router.get("/test", function(req, res) {
+
+    if (!validate(req, res, { nodes: { type: 'JSON', required: true }, project: { required: true }, git: { required: true }, app: { required: true } })) return;
+
+    var nodes = JSON.parse(req.query.nodes),
+        project = req.query.project,
+        git = req.query.git,
+        app = req.query.app;
+
+    getNodesStatus(nodes, function(data) {
+        var ips = object_values(data.nodes).map(function(o) {
+            return o.ip;
+        }).join(',');
+
+        //console.log("ip list", ips)
+        for (var i in data.nodes) {
+            node = data.nodes[i];
+            var cmd = 'cd deployment/docker;./init -g=' + git + ' -a=' + app + ' -p=' + project + ' -n=' + i + ' -i=' + ips + " --tendermintPort=46656 --proxyPort=46658 --appPort=46659";
+            //var cmd = "touch testfile";
+            console.log("Running ssh on:", node, "cmd: ", cmd);
+            //exec("ssh ec2-user@" + data.nodes[i].IP + '  "init -g=' + git + ' -e=' + app + ' -p=' + project + ' -n=' + node + ' -ips=' + ips + '"');
+            exec("ssh -oStrictHostKeyChecking=no ec2-user@" + data.nodes[i].ip + ' "' + cmd + '"', function(e, stdout, stderr) {
+                if (e) console.error(e, stdout, stderr);
+            });
+            // exec("ssh ec2-user@" + data.nodes[i].IP + ' -m ' //
+            //     + '"./deployment/docker/init -g=' + git + ' -a=' + app + ' -p=' + project + ' -n=' + node + ' --tendermintPort=46656 --proxyPort=46658 --appPort=46659 -i=' + ips + '"')
+        }
+    });
+    res.send("done");
 });
 
 /**
@@ -191,7 +237,7 @@ router.get('/remove', function(req, res) {
 
     exec(TERRAFORMAPPLY_CMD, function(err, stdout, stderr) {
         if (err) {
-            return console.log(err);
+            return console.error(err);
         }
         //console.log("Terraform output", stdout, err, stdout);
     });
@@ -280,6 +326,16 @@ function validate(req, res, cfg) {
         }
     }
     return true;
+}
+/**
+ * Utils
+ */
+function object_values(o) {
+    var ret = []
+    for (var i in o) {
+        ret.push(o[i]);
+    }
+    return ret;
 }
 
 module.exports = router;
